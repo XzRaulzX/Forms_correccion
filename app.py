9b2af9385c330b957db457e421d4120a356bfc47
Raul <raulantonioserra@gmail.com>
2026-01-22 19:14:49 +0100

    Push de se

--- app.py
+++ app.py
@@ -5,18 +5,25 @@ from google.oauth2.service_account import Credentials
 import googleapiclient.discovery
 import datetime
 from google.oauth2.service_account import Credentials
-import json
-import os
 from google.oauth2.service_account import Credentials
 # --- CONFIGURACIÓN ---
 FOLDER_ID = "1bN9bMKTFH_Gt7yqdjhmamhj87ZAn2991"  # ID de la carpeta de Google Drive
 SCOPES = ["https://www.googleapis.com/auth/drive.readonly",
           "https://www.googleapis.com/auth/spreadsheets"]
 
-creds_json = st.secrets["GOOGLE_CREDS"]
-
-creds_dict = json.loads(creds_json)
-
+creds_dict = {
+  "type": "service_account",
+  "project_id": "serene-utility-485116-s6",
+  "private_key_id": "9e67b455097bf7659e90cc0d4c212f63aff5a812",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDQIP4qIn9lgpWL\ntI4Ysk2h9KDG03iVL40C33Y/nATpZQKQKuOgSI4Lsh6Q/DF0qvRfCsUns7XNwit5\nFikrpFrHm0mF4y3PYWNitC8unta+ylqWN3wIrQ8fD/pM9JTh9iFK+FkrWzO5MLZ0\nNbAtZaHELQbGY/CyHmnYaH4fiYKvzSQ4vwLLq1DgSSZWjXZrpHmfaNQonkN5VYp0\n7INT7UiZf4l2B0eB01mZaVmlZs+noWS+CAWBY3Xh0P9BY4Dz3/+5I9oeCGIsbdrC\nz6f5XE3Hm2zJQ+D7Wm1nrIDz7jy+Wa1881AjSuDIEPQWeBEPKARWkCTR216nNKo0\nX/jPxxA7AgMBAAECggEAXsDOhcc74a6AP+C9GnA+mL+i3LIVATJrS8YJcy8oK0ux\nzYnBJM0zYg6/DLnMGDXmWEiydn9CA0FlglR8/OHv9FT1tY82YWYQlbS7kEl/MdA1\nSpNLFDYZnsYq6ZMmHvNrt9J14h+83hBX2HNC2IAfFA19up9wSt8+x+fWl6wGg9sa\nRL37Ann6O7xn83LFGQUJRVxj4lDkr+ErOdvOTQp2l7qPF6Z3rHwrHNb5OgLAHd/R\nJNdl3YNxg/Lq8j7oaVpq7M7oYM6ZGcusAdMEN2CRQlJcLi3wP7U6UdGzcqrFLoRI\nRjCmiyPJuzdEKWBG4kVl2RlQmoj5WmlSQturGxXGSQKBgQDxGv6uMwtLGH+3qkgp\n5UAa4pGiK/9v4eK+OmCtwQdZ/Ymsm8dTcLn1MKxWEGCTBelPrVIZ0ireuDfBpfv5\nRSrTvRS+KrfeU3ZWFTRX2U767zG76nH+gCq9ZMywbCQOJaaw0AGonEsaMrMKdncp\nXLLzHAuIhbDZT3168IdzwzR5CQKBgQDc/HvvtR+9YlArETwtii3VHjx1sMhqCo04\nIblHAh6jAzzaBgrHnjaL6oY/Jkoz9eCj5qr1Mj2hi5r1I17BaVA4ZwC0tWewzuIG\n3kXVXLPKBhnQglezUcdovGj6XAm6PUY286locsKXmDPc05rRHkA65L/+9QAQL8yp\n1Z0V8XtkIwKBgQCDvBwrUfh5r4kY8RE80uWTyveHhEKs/t7E0WDBjxZVNRJkHTlr\nfcwdC9sdqxUZP8GapziUoyCJUF4mkGp8aC5eQpFy1iRgdXnSzwMqfLGKqeaiphZi\n1+SCVvD/9BY/JJNPSFefqMXgKpNoxBGXp+6eplQm4+Uc1zHYWlOoDKA/AQKBgQDJ\n542893mmaPriPn8DBKrXeya2SOzzpexdCsLjU9Z2DfE9KiQTOkSQFZjOfcdyLgYu\n+gPcvyh8Prc3njdm8zeuML3+XXf9nSf6Kn8Xb/l8bZKMZWqHlgJheTNY+1qP5IgQ\nkROJMMEGDLPl4RgkVEVPCc05vtNt9p5B1cAOWnHojQKBgGnkYRv1b35j3JB0UJ23\nhi49rfxbA//umbmznRldChGo60my8UzpRxfNXeNrKz1sUKqbcEp5rS1UlMUvwKje\nSn4WW3gSDRjExx8yB0IiuyhsC0DKQOGblF6SVa6zQX6ROB790pXnnE3H7PYY3Mlh\na9zTdWoHw4OpkbzAfPqfVKnk\n-----END PRIVATE KEY-----\n",
+  "client_email": "listener-sheet@serene-utility-485116-s6.iam.gserviceaccount.com",
+  "client_id": "106896905130582640780",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/listener-sheet%40serene-utility-485116-s6.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
 
 #Opcion de produccion 
 creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
